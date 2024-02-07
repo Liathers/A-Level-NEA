39ef31b6b29583afaae07cfd8b30420899686ef4
Liath <71844824+Liathers@users.noreply.github.com>
2024-02-07 13:32:01 +0000

    Initial Commit
    This is the second, and current approach for the project

---
+++ GUIFramework/ConnectionWindow.py
@@ -0,0 +1,6 @@
+from GUIFramework.BaseWindow import *
+
+class ConnectionWindow(BaseWindow):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.name = "Connecting"
---
+++ GUIFramework/MainWindow.py
@@ -0,0 +1,6 @@
+from GUIFramework.BaseWindow import *
+
+class MainWindow(BaseWindow):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.name = "Home"
